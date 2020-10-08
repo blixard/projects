bdd047c0c3f2e33cd7dd673d82515aa93e1af196
Shubha <rupelpadhy@gmail.com>
2020-10-08 20:59:45 +0530

    suncard v-1.0

--- suncard/scripts/game.js
+++ suncard/scripts/game.js
@@ -10,13 +10,17 @@ var comp
 var user1
 var user2
 var user3
+var comp_play = 0
 var wincount = 0
 var lostcount = 0
 var count = 0  
 function loadVis(){
-    document.getElementById("home_btn").style.visibility = "hidden"
-    document.getElementById("user").style.visibility = "hidden"
-    document.getElementById("comp").style.visibility = "hidden"
+    document.getElementById("badge_comp").style.visibility = "hidden"
+    document.getElementById("small_cards_comp").style.visibility = "hidden"
+    document.getElementById("comp_space").style.visibility = "hidden"
+    document.getElementById("user_space").style.visibility = "hidden"
+    document.getElementById("badge_user").style.visibility = "hidden"
+    document.getElementById("small_cards_user").style.visibility = "hidden"
 }
 function game(){
     comp = Math.floor(Math.random()*7)
@@ -32,8 +36,10 @@ function game(){
 
 function play(){
     document.getElementById("play_btn").style.visibility = "hidden"
-    document.getElementById("user").style.visibility ="visible"
-    document.getElementById("com").innerHTML = "choose a card"
+    document.getElementById("badge_comp").style.visibility = "visible"
+    document.getElementById("small_cards_comp").style.visibility = "visible"
+    document.getElementById("badge_user").style.visibility = "visible"
+    document.getElementById("small_cards_user").style.visibility = "visible"
     game()
 }
 
@@ -41,9 +47,11 @@ function play(){
 //button1 
 function card_btn1(){
     count ++
+    scc(count)
     var upower = return_power(user1)
     var cpower = return_power(comp)
     display_comp(comp)
+    display_user(user1);
     comp = Math.floor(Math.random()*7)
     var res =""
     console.log("okay so here : " + upower + " " + cpower)
@@ -64,9 +72,11 @@ function card_btn1(){
         res = "tied"
         console.log("tied")
     }
-    document.getElementById("com").innerHTML = res + " you: "+wincount + " comp: "+lostcount
+    document.getElementById("com").innerHTML = res 
+    document.getElementById("com").style.visibility = "visible"
     document.getElementById("card_btn1").style.visibility = "hidden"
-    document.getElementById("comp-card").style.visibility = "visible"
+    document.getElementById("comp_space").style.visibility = "visible"
+    document.getElementById("user_space").style.visibility = "visible"
     if(count==3){
         wincount = 0 
         lostcount = 0
@@ -74,12 +84,16 @@ function card_btn1(){
     }
 
 }
+
+
 //button2 
 function card_btn2(){
     count = count +1
+    scc(count)
     var upower = return_power(user2)
     var cpower = return_power(comp)
     display_comp(comp)
+    display_user(user2);
     comp = Math.floor(Math.random()*7)
     var res =""
     console.log("okay so here : " + upower + " " + cpower)
@@ -100,9 +114,11 @@ function card_btn2(){
         res = "tied"
         console.log("tied")
     }
-    document.getElementById("com").innerHTML = res + " you: "+wincount + " comp: "+lostcount
+    document.getElementById("com").innerHTML = res 
+    document.getElementById("com").style.visibility = "visible"
     document.getElementById("card_btn2").style.visibility = "hidden"
-    document.getElementById("comp-card").style.visibility = "visible"
+    document.getElementById("comp_space").style.visibility = "visible"
+    document.getElementById("user_space").style.visibility = "visible"
     if(count==3){
         wincount = 0 
         lostcount = 0
@@ -113,9 +129,11 @@ function card_btn2(){
 //button3 
 function card_btn3(){
     count ++
+    scc(count)
     var upower = return_power(user3)
     var cpower = return_power(comp)
     display_comp(comp)
+    display_user(user3);
     comp = Math.floor(Math.random()*7)
     var res =""
     console.log("okay so here : " + upower + " " + cpower)
@@ -136,9 +154,11 @@ function card_btn3(){
         res = "tied"
         console.log("tied")
     }
-    document.getElementById("com").innerHTML = res + " you: "+wincount + " comp: "+lostcount
+    document.getElementById("com").innerHTML = res 
+    document.getElementById("com").style.visibility = "visible"
     document.getElementById("card_btn3").style.visibility = "hidden"
-    document.getElementById("comp-card").style.visibility = "visible"
+    document.getElementById("comp_space").style.visibility = "visible"
+    document.getElementById("user_space").style.visibility = "visible"
     if(count==3){
         if(wincount > lostcount ){
             document.getElementById("com").innerHTML = "you won"
@@ -347,4 +367,62 @@ function display_comp(user1){
     }
 
     console.log("gone to image 3")
+}
+
+
+//set user space
+function display_user(user1){
+    if(user1==0){
+        document.getElementById("uimage").src="../suncard/res/sun.jpg"
+        document.getElementById("utitle").innerHTML = "THE SUN"
+        document.getElementById("upower").innerHTML = "power : 9000"
+    }
+    else if(user1==1){
+        document.getElementById("uimage").src="../suncard/res/dimond_moon.png"
+        document.getElementById("utitle").innerHTML = "dimond moon"
+        document.getElementById("upower").innerHTML = "power : 1000"
+    }
+    else if(user1==2){
+        document.getElementById("uimage").src="../suncard/res/golden_moon.png"
+        document.getElementById("utitle").innerHTML = "golden moon"
+        document.getElementById("upower").innerHTML = "power : 500"
+    }
+    else if(user1==3){
+        document.getElementById("uimage").src="../suncard/res/silver_moon.png"
+        document.getElementById("utitle").innerHTML = "silver moon"
+        document.getElementById("upower").innerHTML = "power : 300"
+    }
+    else if(user1==4){
+        document.getElementById("uimage").src="../suncard/res/rock_moon.png"
+        document.getElementById("utitle").innerHTML = "rock moon"
+        document.getElementById("upower").innerHTML = "power : 100"
+    }
+    else if(user1==5){
+        document.getElementById("uimage").src="../suncard/res/half_moon.png"
+        document.getElementById("utitle").innerHTML = "half moon"
+        document.getElementById("upower").innerHTML = "power : 50"
+    }
+    else if(user1==6){
+        document.getElementById("uimage").src="../suncard/res/dark_moon.png"
+        document.getElementById("utitle").innerHTML = "dark moon"
+        document.getElementById("upower").innerHTML = "power : -100"
+    }
+
+    console.log("gone to user space")
+}
+
+
+
+//computer small cards remover
+
+function scc(inp){
+    if(inp==1){
+        document.getElementById("sc1").style.visibility= "hidden"
+    }
+    else if(inp==2){
+        document.getElementById("sc2").style.visibility= "hidden"
+    }
+    else if(inp==3){
+        document.getElementById("sc3").style.visibility= "hidden"
+    }
 }
