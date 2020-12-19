c746feb02fabac320013f660ea942114b87eb9b2
Shubha <rupelpadhy@gmail.com>
2020-12-19 13:22:00 +0530

    added bot

---
+++ bot/chat/script/index.js
@@ -0,0 +1,32 @@
+var replies = [
+    "name:I am stellarBot ",
+    "def:ghi"
+];
+
+function fun(){
+    var s = document.getElementById("userIp").value;
+    fun2(s);
+}
+
+function fun2(s){
+    var i
+    for(i= 0; i<replies.length ; i++){
+        var x = replies[i].split(":");
+        if(x[0] == s){
+            console.log("found");
+            document.getElementById("output").innerHTML=x[1];
+            break;
+        }
+    }
+    if(i==replies.length){
+        document.getElementById("output").innerHTML="I counldn understand :( ";
+    }
+}
+
+//question - (wh s,how)
+//each question has sub groups
+//if not question - command / talking 
+//comand - find the verb if it is in directory and sentence and do it 
+//talking , try find the closest sentence available in directory to find a reply
+//introductions
+//return google search of it if nothing else 
