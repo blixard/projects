03fc77190b508079e1b366902abf728866e16af8
Shubha <rupelpadhy@gmail.com>
2020-12-29 19:59:11 +0530

    reg changes

--- reg/scripts/fun.js
+++ reg/scripts/fun.js
@@ -2,6 +2,9 @@ function find(){
    var exp = document.getElementById("exp").value
    var text = document.getElementById("text").value
    var res = text.match(exp)
+   if(res==null){
+       res = "no matches, Nope "
+   }
    document.getElementById("op").innerHTML = res
    console.log(res);
 }
