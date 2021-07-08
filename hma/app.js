4e9ebd12880b1c83159dde34bda3af9c77c44e55
Shubha <rupelpadhy@gmail.com>
2021-07-08 14:08:05 +0530

    marrige aniverseery project added

---
+++ hma/app.js
@@ -0,0 +1,168 @@
+// gsap.to(".box", {x: 100, duration: 1});
+var playing = false;
+window.addEventListener("DOMContentLoaded", (e)=>{
+    var imgs = document.images,
+    len = imgs.length,
+    counter = 0;
+
+    [].forEach.call( imgs, function( img ) {
+        if(img.complete)
+        incrementCounter();
+        else
+        img.addEventListener( 'load', incrementCounter, false );
+    } );
+
+    function incrementCounter() {
+        counter++;
+        if ( counter === len ) {
+            var loader = document.getElementById("loading")
+            loader.remove()
+            console.log( 'All images loaded!' );
+            animatePage1()
+            animatePage2()
+            animatePage3()
+            animatePage4()
+        }
+    }
+
+})
+
+if(document.readyState=='loading'){
+    console.log("loading")
+}
+
+animatePage1 = ()=>{
+    var tl = gsap.timeline(
+        {
+            scrollTrigger:{
+                duration:2,
+                trigger:".page1",
+                markers:true,
+                start:"top",
+                end:"+=2000",
+                scrub:"1"
+            },
+        }
+    )
+    tl.from(".img1",{
+        xPercent:-10,
+        rotate:10
+    })
+    tl.from(".img2",{
+        yPercent:20,
+    })
+    tl.from(".img3",{
+        xPercent:-30
+    })
+    tl.from(".img4",{
+        yPercent:-25
+    })
+    tl.from(".img5",{
+        xPercent:30
+    })
+    tl.from(".img6",{
+        yPercent:-35
+    })
+    tl.from(".img7",{
+        xPercent:-30,
+        rotate:60
+    })
+    tl.from(".img8",{
+        yPercent:-10,
+        xPercent:-40
+    })
+    tl.from(".mid",{
+        xPercent:-10
+    })
+    tl.from(".img9",{
+        xPercent:-10
+    })
+    tl.from(".img10",{
+        yPercent:-10,
+        xPercent:-20
+    })
+    tl.from(".img11",{
+        xPercent:-30
+    })
+    tl.from(".img12",{
+        yPercent:-40
+    })
+}
+
+animatePage2 = ()=>{
+    var tl = gsap.timeline({
+        scrollTrigger:{
+            duration:2,
+            trigger:".page2",
+            markers:false,
+            start:"top bottom",
+            end:"+=1000",
+            scrub:"1"
+        },
+    })
+    tl.from("#p2head",{
+        opacity:0
+    },"-=1")
+    tl.from("#p2main",{
+        opacity:0
+    },"-=0.8")
+}
+
+animatePage3 = ()=>{
+    var tl = gsap.timeline({
+        scrollTrigger:{
+            duration:1,
+            trigger:".page3",
+            markers:false,
+            start:"top bottom",
+        },
+    })
+    tl.from("#formc",{
+        opacity:0
+    })
+    tl.from("#p3main",{
+        opacity:0
+    })
+}
+
+animatePage4 = ()=>{
+    var tl = gsap.timeline({
+        scrollTrigger:{
+            trigger:".page4",
+            start:"top bottom",
+            scrub:true
+        }
+    })
+    tl.from("#p4head",{
+        opacity:0
+    })
+    tl.from(".teamview",{
+        opacity:0
+    },"-=0.4")
+}
+
+var flag = false;
+function fun(){
+    if(flag == false){
+        document.querySelector(".menu").style.left=0;
+        flag = true;
+    }
+    else if(flag == true){
+        document.querySelector(".menu").style.left="-100%";
+        flag = false;
+    }
+}
+
+function playvid(){
+   var vid =  document.getElementById("v1");
+   if(playing == false){
+    vid.play()
+    playing = true
+   }
+   else{
+       vid.pause()
+       playing = false
+   }
+   
+   console.log("yooooo")
+}
